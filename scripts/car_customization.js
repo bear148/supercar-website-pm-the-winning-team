3f721d494741f42a5aa4dc116f15abe6f01a22a8
bear148 <bizzeebear@hotmail.com>
2024-12-06 15:48:18 -0800

    Finished customization, began merchandise

--- scripts/car_customization.js
+++ scripts/car_customization.js
@@ -1,5 +1,9 @@
-var root = document.getElementById("customization-root");
+var colors = ['#220000', '#00ff00', '#0000ff', '#330000', '#00aa00', '#0000aa', '#440000', '#00ee00'];
+var random = colors[Math.floor(Math.random() * colors.length)];
 
-function stitching() {
-    console.log("Stitching menu accessed");
+var color_options = document.getElementsByClassName('color');
+
+for (var i = 0; i < colors.length; i++) {
+    color_options[i].style.background = random;
+    console.log("Color");
 }

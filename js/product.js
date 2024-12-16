5b27c4aabeb25b2c5f5500ee26edd80ca7872c19
bear148 <bizzeebear@hotmail.com>
2024-12-16 15:05:19 -0800

    Fixed everything no longer looks zoomed in

--- js/product.js
+++ js/product.js
@@ -1,11 +1,31 @@
+var quantity = 1;
+var price = 24.99;
+var totalElem = document.getElementById("totalh2");
+var prompt = "Total: $";
+
 function parent(e) {
     return e.parentElement;
 }
 
 function add_item(e) {
-    console.log("Add to quantity: " + parent(e));
+    quantity++;
+    parent(e).children[1].innerHTML = quantity;
+
+    price = price + 24.99;
+    document.getElementById("totalh2").innerHTML = prompt + price.toFixed(2);
+    document.getElementsByClassName("product-header").children[1] = price.toFixed(2);
+
+    console.log(price);
 }
 
 function sub_item(e) {
-    console.log("Sub quantity: " + parent(e));
+    if(quantity > 0) {
+        quantity--;
+        parent(e).children[1].innerHTML = quantity;
+        price = price - 24.99;
+    }
+
+    document.getElementById("totalh2").innerHTML = prompt + price.toFixed(2);
+
+    console.log(price);
 }
